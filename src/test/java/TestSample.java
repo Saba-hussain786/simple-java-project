16019d490bf794967952bd4bcebc7b08acee4712
mkonudula321 <jira1001@gmail.com>
2017-04-21 08:33:51 +0530

    Update TestSample.java

--- src/test/java/TestSample.java
+++ src/test/java/TestSample.java
@@ -9,7 +9,7 @@ public class TestSample extends Assert {
   @Test
   public void firstTest(){
    Assert.assertTrue("test passed", 2>1);
-    
-    //system.out.print();
+  
+    system.out.print();
   }
 }
