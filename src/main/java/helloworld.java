1eca63f8f57fa9be36f49703d4e85eeb45facafa
Saba-hussain786 <72196383+Saba-hussain786@users.noreply.github.com>
2020-10-01 15:37:58 +0530

    Update helloworld.java

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -3,12 +3,12 @@
  */
 import org.apache.log4j.Logger;
 
-public class helloworld {
-  Logger logger;
-  public static void main(String[] args){
+public class helloworl{ //Class
+  Logger logger;  //Member Variable
+  public static void main(String[] args){ //Main method(Java program execution starts from here).JVM automatically calls main method.
 
-    System.out.println("Hello wolrd");
-    System.out.println("Git merge testing00");
+    System.out.println("Hello wolrd"); //Print statement1(main method is invoked).
+    System.out.println("Git merge testing00"); //Print statement2(main method is invoked).
     
   }
 }
